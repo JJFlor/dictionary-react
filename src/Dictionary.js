c7998bc1aeb99fbaf403c0a10a7c8f11470a20c9
Júlia Jaile Flores <73072636+JJFlor@users.noreply.github.com>
2021-05-15 17:25:06 +0200

    Added photo API and styled to the photos shown

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,33 @@
 import React,{ useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props){
     let [keyWord, setKeyWord] = useState(props.defaultKeyWord);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos]= useState(null);
     
-    function handleResponse(response){
+    function handleDictionaryResponse(response){
         setResults(response.data[0]);
+    }
+
+    function handleImagesResponse (response){
+        setPhotos(response.data.photos);
     }
 
     function search(){
         //documentation: https://dictionaryapi.dev/
         let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyWord}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+
+        //documentation: https://www.pexels.com/api/documentation/
+        const pexelsApiKey = "563492ad6f917000010000012c9b635b7a1d4cff8fa2a7b8a4204d0d";
+        let pexelsApiUrl=`https://api.pexels.com/v1/search?query=${keyWord}&per_page=8`;
+        let headers = {"Authorization" : `Bearer ${pexelsApiKey}`};
+        axios.get(pexelsApiUrl, { headers }).then(handleImagesResponse);
     }
 
     function handleSubmit(event){
@@ -54,14 +66,11 @@ export default function Dictionary(props){
                </div>
              </section>
              <Results results={results}/>
+             <Photos photos={photos} />
            </div>
     );
     } else {
         load();
         return "Loading...";
     }
-
-
-
-   
 }
